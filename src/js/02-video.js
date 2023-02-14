9c18e32e7c80ca396b4b67628a62c8b21ac6ad6a
Ihor Karen <karenihor@gmail.com>
2023-02-14 23:47:02 +0200

    vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,8 @@ import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player');
 
-const currentTimeKey = 'videoplayer-current-time';
-
 player.on('timeupdate', throttle(function(data) {
-  localStorage.setItem(currentTimeKey, JSON.stringify(data.seconds));
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
 }, 1000));
 
-player.setCurrentTime(localStorage.getItem(currentTimeKey));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
